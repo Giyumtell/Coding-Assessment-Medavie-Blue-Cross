69faea4bbea2cca96cfd555193c88e6f4edba274
Masoud Torkian <giyumtell.spectre@gmail.com>
2022-07-27 23:00:32 -0400

    Tester implemented

--- src/Tester.java
+++ src/Tester.java
@@ -1,4 +1,31 @@
-
+import java.util.Scanner;
 public class Tester {
-
+	public static void main(String[] args) {
+		int selection = 1;
+		Scanner in = new Scanner(System.in);
+		CeilingFan fan = new CeilingFan();
+		while(selection != 0) {
+			System.out.println("------->Tester for Ceiling fan functionalities<-------");
+			System.out.println("-1 : pull the speed cord");
+			System.out.println("-2 : pull the direction cord");
+			System.out.println("-3 : get current status of the fan");
+			System.out.println("-0 : End");
+			System.out.print("Selection : ");
+			selection = in.nextInt();
+			switch (selection){
+				case 1:
+					fan.speedCord();
+					break;
+				case 2:
+					fan.directionCord();
+					break;
+				case 3:
+					System.out.println("\n"+fan.toString()+"\n");
+					break;
+				default:
+					break;
+			}
+		}
+		System.out.println("------->End of testing<-------");
+	}
 }
